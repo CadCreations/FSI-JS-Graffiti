371b2500e66ecfc7e00ce77d95b60c9e3cf91b4a
CadCreations <davescottmay@gmail.com>
2022-01-26 20:21:48 -0500

    changes

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let header = document.querySelector('#page-header')
+//console.log(header)
+header.style.textAlign = "left"
 
-// Select some elements...
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+console.log(dogName)
+

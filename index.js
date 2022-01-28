e7b1ccd07164445e24d2472cdd6448b49e17dd58
CadCreations <davescottmay@gmail.com>
2022-01-28 10:31:12 -0500

    update Jan 28

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 let header = document.querySelector('#page-header')
 //console.log(header)
-header.style.textAlign = "left"
+//header.style.textAlign = "center"
+//header.style.color = 'red'
 
 
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
-    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.borderRadius = '20px'
 }
 
-let dogName = document.querySelectorAll('.dog-name')
-console.log(dogName)
+//get eleement by tag
+let myTest = document.getElementsByTagName('p')
+
+myTest[0].style.color = "purple"
+
+
+let dogName = document.querySelectorAll(".dog-name")
+
+//console.log(dogName)
+//dogName[0].style.color = 'red'
+
+
+dogName[0].style.color = 'red'
+dogName[1].style.color = 'blue'
+dogName[2].style.color = 'brown'
+//console.log(dogName[x])
+
+let dogOne = document.querySelectorAll('p')
+//console.log(dogOne[0])
+//dogOne[0].style.color = 'red'
+//dogOne[1].style.color = 'blue'
+//dogOne[2].style.color = 'brown'
+
+
+
+
+
+
+
+
 
